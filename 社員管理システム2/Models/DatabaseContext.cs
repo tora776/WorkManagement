87b9e7d6fe95490c7b59c3bb7ff7fd7d1707b68f
tora776 <taichi.nakaoka5757@gmail.com>
2024-08-04 11:04:44 +0900

    20240804_DB接続作成（テスト未実施）

---
+++ 社員管理システム2/Models/DatabaseContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Identity.Client;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace 社員管理システム2.Models
+{
+    public class DatabaseContext
+    {
+        private NpgsqlConnection conn;
+        private NpgsqlTransaction transaction;
+        public void connectDB()
+        {
+            //接続文字列
+            string conn_str = "Server=localhost;Port=5432;User ID=dbo;Database=employeedb;Password=jigs&t4d;Enlist=true";
+
+            //TransactionScopeの利用
+            using (TransactionScope ts = new TransactionScope())
+            {
+                using (this.conn = new NpgsqlConnection(conn_str))
+                {
+                    //PostgreSQLへ接続
+                    conn.Open();
+                }
+
+            }
+        }
+
+        public void disconnectDB(){
+            conn.Close();
+            conn.Dispose();
+        }
+    }
+}
+
+
---
+++ 社員管理システム2/Models/model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 社員管理システム2
+{
+    internal class Employees
+    {
+        public char EmployeeID { set; get; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public string FirstNameKana {  set; get; }
+        public string LastNameKana { set; get; }
+        public string Email {  set; get; }
+        public char PhoneNumber {  set; get; }
+        public DateTime HireDate { set; get; }
+        public int Department {  set; get; }
+        public int Positon { set; get; }
+        public int Status { set; get; }
+        }
+    }
